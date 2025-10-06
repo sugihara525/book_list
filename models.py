c0f9f8d974d4bc16398efdaf1e6cb4c93e35890f
sugihara525 <re.sugihara525@gmail.com>
2025-10-06 22:29:38 +0900

    fix

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
